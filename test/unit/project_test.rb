5b8557a930066d3b9e04bdff4015e5f27c84dd2c
shingara <cyril.mougel@gmail.com>
2008-04-12 14:15:03 +0200

    ajout du test de creation de projet vide

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,8 +1,19 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
+  fixtures :projects
+
+  def setup
+    @project = Project.new
+  end
+
+  def teardown
+  end
+
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_presence_title
+    assert_equal 2, Project.count
+    @project.save
+    assert_equal 2, Project.count
   end
 end
