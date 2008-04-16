83cf1a3492de22aef59e6ff42b27c41b10c8a575
shingara <cyril.mougel@gmail.com>
2008-04-16 21:52:19 +0200

    fix mauvais test fonctionel

--- test/functional/tasks_controller_test.rb
+++ test/functional/tasks_controller_test.rb
@@ -2,44 +2,44 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class TasksControllerTest < ActionController::TestCase
   def test_should_get_index
-    get :index
+    get :index, :project_id => 1
     assert_response :success
     assert_not_nil assigns(:tasks)
   end
 
   def test_should_get_new
-    get :new
+    get :new, :project_id => 1
     assert_response :success
   end
 
   def test_should_create_task
     assert_difference('Task.count') do
-      post :create, :task => {:title => 'title', :description => 'description', :project_id => 1 }
+      post :create, :project_id => 1, :task => {:title => 'title', :description => 'description', :project_id => 1 }
     end
 
-    assert_redirected_to task_path(assigns(:task))
+    assert_redirected_to project_task_path(1, assigns(:task))
   end
 
   def test_should_show_task
-    get :show, :id => tasks(:one).id
+    get :show, :project_id => 1, :id => tasks(:one).id
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :id => tasks(:one).id
+    get :edit, :project_id => 1, :id => tasks(:one).id
     assert_response :success
   end
 
   def test_should_update_task
-    put :update, :id => tasks(:one).id, :task => { }
-    assert_redirected_to task_path(assigns(:task))
+    put :update, :project_id => 1, :id => tasks(:one).id, :task => { }
+    assert_redirected_to project_task_path(1, assigns(:task))
   end
 
   def test_should_destroy_task
     assert_difference('Task.count', -1) do
-      delete :destroy, :id => tasks(:one).id
+      delete :destroy, :project_id => 1, :id => tasks(:one).id
     end
 
-    assert_redirected_to tasks_path
+    assert_redirected_to project_tasks_path(1)
   end
 end
